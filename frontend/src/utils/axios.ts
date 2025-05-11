b52187aa94c499bfad91d8ee5e8b8b804d3f0a6d
GayanthaSandeep16 <gayantha.20210118@iit.ac.lk>
2025-05-11 12:35:34 +0530

    fix: Update API base URL from localhost:4500 to localhost:3030

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4500/api',
+  baseURL: 'http://localhost:3030/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
